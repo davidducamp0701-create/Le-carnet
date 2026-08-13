022524a46eed7330117f8e4678307135692ebee0
davidducamp0701-create <davidducamp0701@gmail.com>
2026-08-13 05:25:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { nanoid } = require("nanoid");
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname)));
 
 const DATA_FILE = path.join(__dirname, "data.json");
 
